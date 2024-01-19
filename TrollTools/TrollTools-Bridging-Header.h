46ee5d23162705e0aec3eebc0b641191f67d3909
c22dev <102235607+c22dev@users.noreply.github.com>
2024-01-19 11:23:34 +0100

    Delete TrollTools directory

--- TrollTools/TrollTools-Bridging-Header.h
+++
@@ -1,9 +0,0 @@
-//
-//  Use this file to import your target's public headers that you would like to expose to Swift.
-//
-
-#import "TSUtil.h"
-#import "LocSimPrivateHeaders.h"
-#include "RemoteLog.h"
-
-#import "grant_full_disk_access.h"
